b7a1bdbcc88a0b108efee837a20429dfb624075b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-19 15:21:26 -0500

    fix: ignore "link:" and "file:" dependencies

--- src/make-exact.js
+++ src/make-exact.js
@@ -2,13 +2,14 @@ let importFrom = require('import-from')
 
 let isExactRE = /(^|@)[0-9]/
 let nonExactRE = /(^|@)[~^].+/
+let isLocalRE = /^(link|file):/
 
 function makeExact(pkg, cwd = process.cwd()) {
   let changed = false
   let makeExact = dependencies =>
     dependencies &&
     Object.entries(dependencies).forEach(([name, allowedVersion]) => {
-      if (!isExactRE.test(allowedVersion)) {
+      if (!isExactRE.test(allowedVersion) && !isLocalRE.test(allowedVersion)) {
         let installedVersion = importFrom(cwd, name + '/package.json').version
         if (installedVersion) {
           changed = true
